1c75c58fbfda8809ca7519afd0a6d8b8aae65a6f
dcamppos83 <danielcamppos@gmail.com>
2022-08-17 10:18:39 -0300

    update

--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -2,9 +2,13 @@
 #cis-kubelet.sh
 
 #total_fail=$(kube-bench run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json | jq .[].total_fail)
-total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets node --version 1.19 --check 4.2.1,4.2.2 --json | jq .Totals.total_fail)
+total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets node --version 1.19 --check 4.2.1,4.2.2 --exit-code 1 --json | jq .Totals.total_fail)
 
-if [[ {$total_fail} == 1 ]];
+# Para uso com docker apenas
+    exit_code=$?
+    echo "Exit Code : $exit_code"
+
+if [[ {$exit_code} == 1 ]];
         then
                 echo "CIS Benchmark Failed Kubelet while testing for 4.2.1, 4.2.2"
                 exit 1;
