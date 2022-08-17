14c891ac9d69ac7642fdbf65adca4a52606178ff
dcamppos83 <danielcamppos@gmail.com>
2022-08-17 10:13:26 -0300

    update

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -4,10 +4,23 @@
 #total_fail=$(kube-bench run --targets etcd  --version 1.15 --check 2.2 --json | jq .[].total_fail)
 total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets etcd --version 1.19 --check 2.2 --json | jq .Totals.total_fail)
 
-if [[ "$total_fail" -ne 0 ]];
+# Para uso com docker apenas
+    exit_code=$?
+    echo "Exit Code : $exit_code"
+
+if [[ {$total_fail} == 1 ]];
         then
                 echo "CIS Benchmark Failed ETCD while testing for 2.2"
                 exit 1;
         else
                 echo "CIS Benchmark Passed for ETCD - 2.2"
 fi;
+
+
+# if [[ "$total_fail" -ne 0 ]];
+#         then
+#                 echo "CIS Benchmark Failed ETCD while testing for 2.2"
+#                 exit 1;
+#         else
+#                 echo "CIS Benchmark Passed for ETCD - 2.2"
+# fi;
--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -4,11 +4,18 @@
 #total_fail=$(kube-bench run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json | jq .[].total_fail)
 total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets node --version 1.19 --check 4.2.1,4.2.2 --json | jq .Totals.total_fail)
 
-
-if [[ "$total_fail" -ne 0 ]];
+if [[ {$total_fail} == 1 ]];
         then
                 echo "CIS Benchmark Failed Kubelet while testing for 4.2.1, 4.2.2"
                 exit 1;
         else
                 echo "CIS Benchmark Passed Kubelet for 4.2.1, 4.2.2"
 fi;
+
+# if [[ "$total_fail" -ne 0 ]];
+#         then
+#                 echo "CIS Benchmark Failed Kubelet while testing for 4.2.1, 4.2.2"
+#                 exit 1;
+#         else
+#                 echo "CIS Benchmark Passed Kubelet for 4.2.1, 4.2.2"
+# fi;
